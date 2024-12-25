819720d743abe3716fc335ae6ae1fba4c7dc3b9e
Irostovsky <ivan.rostovsky@gmail.com>
2024-12-25 12:19:37 +0200

    Use dispatch redux hook.

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,8 +1,13 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
+  const dispath = useDispatch();
+
+  const incrementHandler = () => dispath({ type: "increment" });
+
+  const decrementHandler = () => dispath({ type: "decrement" });
 
   const toggleCounterHandler = () => {};
 
@@ -10,6 +15,10 @@ const Counter = () => {
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
